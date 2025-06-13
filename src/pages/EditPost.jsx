4fa385ca708f9dd388813036d68d83b7976218b1
Mobolaji Samuel <141836780+Kingmoballs@users.noreply.github.com>
2025-06-13 13:56:50 +0100

    Fixed useEffect dependencies

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -35,7 +35,7 @@ export default function EditPost() {
         };
 
         fetchPost();
-    }, []);
+    }, [id]);
 
 
     const handleChange = (e) => {
--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -32,7 +32,7 @@ export default function SinglePost() {
 
         fetchPost();
         fetchCurrentUser();
-    }, []);
+    }, [id]);
 
 
     const handleDelete = async () => {
